4e0696dce8ca536ddcaaec380291135bab3deb6c
Gentrit Uka <gentrit-uka@live.com>
2022-06-16 17:28:07 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// for (let user of users){
+//     alert(user.id + "/n" + user.name + '/n' + user.address.city)
+// }
+
+// console.log("users: ", window.users);
+
+// console.log("todos: ", window.todos);
